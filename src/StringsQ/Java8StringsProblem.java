9bb1145bc5eabe1826a88e9c4e693095c67ef4e6
elahe <elahehseyedain93@gmail.com>
2023-08-28 19:52:35 +0100

    Implementing Java8StringProblems part1.

---
+++ src/StringsQ/Java8StringsProblem.java
@@ -0,0 +1,180 @@
+package StringsQ;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Java8StringsProblem {
+    private static String reverseString(String name) {
+        char[] chars = name.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            char temp = chars[i];
+            chars[i] = chars[chars.length - i - 1];
+            chars[chars.length - i - 1] = temp;
+        }
+        return new String(chars);
+    }
+
+    private static StringBuilder reverseString1(String name) {
+        char[] chars = name.toCharArray();
+        StringBuilder reversed = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            reversed.append(chars[i]);
+        }
+        return reversed;
+    }
+
+    private static String reverseString2(String name) {
+        char[] chars = name.toCharArray();
+        StringBuilder reversed = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            reversed.append(chars[i]);
+        }
+        return reversed.toString();
+    }
+
+    //worstcase
+    private static Set<Character> findDuplicate(String name) {
+        Set<Character> duplicate = new LinkedHashSet<>();
+        for (int i = 0; i < name.length(); i++) {
+            for (int j = i + 1; j < name.length(); j++) {
+                if (name.charAt(i) == name.charAt(j))
+                    duplicate.add(name.charAt(i));
+            }
+        }
+        return duplicate;
+    }
+
+    //O(n)
+    private static Set<Character> findDuplicateUsingArray(String name) {
+        Set<Character> duplicate = new LinkedHashSet<>();
+        int[] arrayForChar = new int[256];
+
+        for (int i = 0; i < name.length(); i++) {
+            arrayForChar[name.charAt(i)] = arrayForChar[name.charAt(i)] + 1;
+        }
+        for (int i = 0; i < arrayForChar.length; i++) {
+            if (arrayForChar[i] > 1)
+                duplicate.add((char) i);
+        }
+
+        return duplicate;
+    }
+
+    private static Set<Character> findDuplicateUsingHashMap(String name) {
+        HashMap<Character, Integer> countMap = new HashMap<>();
+        Set<Character> duplicate = new HashSet<>();
+        for (int i = 0; i < name.length(); i++) {
+            if (countMap.containsKey(name.charAt(i))) {
+                countMap.put(name.charAt(i), countMap.get(name.charAt(i)) + 1);
+            } else {
+                countMap.put(name.charAt(i), 1);
+            }
+
+            for (Map.Entry<Character, Integer> entry : countMap.entrySet()) {
+                if (entry.getValue() > 1) {
+                    duplicate.add(entry.getKey());
+                }
+            }
+        }
+        return duplicate;
+    }
+
+    public static void main(String[] args) {
+        String name = "elaheh";
+        String name1 = new String("elaheh");
+        System.out.println(name == name1);
+        System.out.println(name.equals(name1));
+
+        //add string from heap to string pool
+        String name3 = name1.intern();
+        System.out.println(name3 == name);
+
+
+        System.out.println(reverseString(name));
+        System.out.println(reverseString1(name));
+
+        //It proves String is immutable, because instead of change the content of s1, create new String.
+        String s1 = "qqq";
+        System.out.println(s1.hashCode());
+        s1 = s1 + "yyy";
+        System.out.println(s1.hashCode());
+
+        System.out.println(findDuplicate(name));
+        System.out.println(findDuplicateUsingArray(name));
+        System.out.println(findDuplicateUsingHashMap(name));
+        HashMap<Character, Integer> countMap = new HashMap<>();
+        countMap.put('k', 0);
+        countMap.put('r', 1);
+        countMap.put('f', 2);
+        System.out.println("*********************************************************");
+        Integer value = countMap.get('k');
+    }
+
+
+    @Test
+    public void testReverseString() {
+        //Test with String that has even length
+        String evenString = "abcd";
+        String reversedEevenString = "dcba";
+        StringBuilder reversedEvenStringBuilder = new StringBuilder("dcba");
+        assertEquals(reversedEevenString, Java8StringsProblem.reverseString(evenString));
+        assertEquals(reversedEvenStringBuilder.toString(), Java8StringsProblem.reverseString1(evenString).toString());
+        assertEquals(reversedEevenString, Java8StringsProblem.reverseString2(evenString));
+
+        //Test with String that has odd length
+        String oddString = "abcde";
+        String reversedOddString = "edcba";
+        StringBuilder reversedOddStringBuilder = new StringBuilder("edcba");
+        assertEquals(reversedOddString, Java8StringsProblem.reverseString(oddString));
+        assertEquals(reversedOddStringBuilder.toString(), Java8StringsProblem.reverseString1(oddString).toString());
+        assertEquals(reversedOddStringBuilder.toString(), Java8StringsProblem.reverseString2(oddString));
+
+        //Test with empty String
+        String empty = "";
+        String reversedEmpty = "";
+        StringBuilder reversedEmptyBuilder = new StringBuilder("");
+        assertEquals(reversedEmpty, Java8StringsProblem.reverseString(empty));
+        assertEquals(reversedEmptyBuilder.toString(), Java8StringsProblem.reverseString1(empty).toString());
+        assertEquals(reversedEmpty, Java8StringsProblem.reverseString2(empty));
+
+        //Test with Single Character
+        String single = "x";
+        String reversedSingle = "x";
+        StringBuilder reversedSingleBuilder = new StringBuilder("x");
+        assertEquals(reversedSingle, Java8StringsProblem.reverseString(single));
+        assertEquals(reversedSingleBuilder.toString(), Java8StringsProblem.reverseString1(single).toString());
+        assertEquals(reversedSingle, Java8StringsProblem.reverseString2(single));
+    }
+
+        @Test
+        public void testFindDuplicateUsingHashMap() {
+            // Test with a string containing duplicate characters
+            String input1 = "hello";
+            Set<Character> expected1 = new HashSet<>();
+            expected1.add('l');
+            expected1.add('o');
+            assertEquals(expected1, Java8StringsProblem.findDuplicateUsingHashMap(input1));
+
+            // Test with a string containing no duplicate characters
+            String input2 = "world";
+            Set<Character> expected2 = new HashSet<>();
+            assertEquals(expected2, Java8StringsProblem.findDuplicateUsingHashMap(input2));
+
+            // Test with an empty string
+            String input3 = "";
+            Set<Character> expected3 = new HashSet<>();
+            assertEquals(expected3, Java8StringsProblem.findDuplicateUsingHashMap(input3));
+
+            // Test with a string containing spaces
+            String input4 = "hello world";
+            Set<Character> expected4 = new HashSet<>();
+            expected4.add('l');
+            expected4.add('o');
+            expected4.add(' ');
+            assertEquals(expected4, Java8StringsProblem.findDuplicateUsingHashMap(input4));
+        }
+    }
+
